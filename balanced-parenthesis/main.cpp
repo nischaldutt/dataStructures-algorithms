e2e2f6641c96d7aa4129eb519443cafbf577c3ab
Nischal <nischaldutt01@gmail.com>
2018-10-23 18:05:21 +0530

    ehancement in  bracket prenthesis

--- balanced-parenthesis/main.cpp
+++ balanced-parenthesis/main.cpp
@@ -2,44 +2,47 @@
 #include<stack>
 #include<string>
 
+using namespace std;
+
 bool isPair(char open, char close) { //checks if pair of opening or closing brackets balanced or not
-    if(open == '(' && close == ')') 
+    if(open == '(' && close == ')')
         return true;
-    else if(open == '{' && close == '}') 
+    else if(open == '{' && close == '}')
         return true;
-    else if(open == '[' && close == ']') 
+    else if(open == '[' && close == ']')
         return true;
-    
+
     return false;
 }
 
-bool isBalanced(string exp) { //scans whole expression to detect brackets
+bool isBalanced(char* exp) { //scans whole expression to detect brackets
     int i;
-    int len = string.length(exp);
+    
     stack<char> S; //create a stack object
 
-    for(i=0;i<len;i++) {
+    while(*exp) {
         if(exp[i] == '(' || exp[i] == '{' || exp[i] == '[') //if opening bracket is detected
             S.push(exp[i]); //push opening bracket to the stack
         else if(exp[i] == ')' || exp[i] == '}' || exp[i] == ']') { //if closing bracket is detected
             if(S.empty() || !isPair(S.top(), exp[i])) //if stack is empty OR top of the stack and closing bracket are not balanced
-                return false; 
+                return false;
             else
                 S.pop(); //pop the opening bracket from top of the stack
         }
+        exp++;
     }
     return S.empty() ? true : false; //check is the stack is empty in the end or not
 }
 
 int main() {
-    string expression;
-    cout<<"Enter the expression to check: ";
-    cin>>expression;
+    char expression[100];
+    std::cout<<"Enter the expression to check: ";
+    std::cin>>expression;
 
     if(isBalanced(expression))
-        return "Balanced";
+        std::cout<<"Balanced";
     else
-        return "Not Balanced";
-    
+        std::cout<<"Not Balanced";
+
     return 0;
 }
